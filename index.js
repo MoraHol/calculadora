057aae8ece7be7ed9fb6d68ac69cdbef80ec2b01
Alexis Holguin <wholguinmor@uniminuto.edu.co>
2019-02-07 17:43:58 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+let screen = document.getElementById('screen')
+let firstNumber = document.getElementById('firstNumber')
+let secondNumber = document.getElementById('secondNumber')
+
+document.getElementById('sum').addEventListener('click', function () {
+  screen.value = sum(parseFloat(firstNumber.value), parseFloat(secondNumber.value))
+})
+document.getElementById('sub').addEventListener('click', function () {
+  screen.value = subtract(parseFloat(firstNumber.value), parseFloat(secondNumber.value))
+})
+
+function sum(x, y) {
+  return x + y
+}
+
+function subtract(x, y) {
+  return x - y
+}
+function multiply(x, y) {
+  if (x == 0 || y == 0) {
+    return 0
+  } else {
+    return x * y
+  }
+}
+
+function divide(x, y) {
+  if (y == 0) {
+    return 'error'
+  }
+  return x / y
+}
